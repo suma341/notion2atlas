9ba576ac5801e4f76bb14b2851d8da61e3027216
sasasanono <ukakau@gmail.com>
2026-01-03 15:51:53 +0900

    add workflow to run horizon-atlas action to build-deploy

---
+++ scripts/runHorizonAtlasBuild.js
@@ -0,0 +1,44 @@
+async function notifyDiscord(message) {
+  const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+  if (!webhookUrl) {
+    console.error("DISCORD_WEBHOOK_URL not set");
+    return;
+  }
+
+  await fetch(webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: "GitHub Actions",
+      content: message,
+    }),
+  });
+}
+
+const token = process.env.HORIZON_ATLAS_APP_GH_PAT;
+
+const res = await fetch(
+  "https://api.github.com/repos/Ryukoku-Horizon/horizon-atlas/actions/workflows/build_Deploy.yml/dispatches",
+  {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/vnd.github+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ref: "main" }),
+  }
+);
+
+if (!res.ok) {
+  const text = await res.text();
+  await notifyDiscord(`
+      notion2atlasからhorizon-atlasのworkflow実行に失敗しました
+      期限切れの可能性があるので、確認しましょう
+      error: ${text}
+    `)
+  console.error(text)
+}
+
+console.log("workflow dispatched");
