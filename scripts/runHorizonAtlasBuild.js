d85d72bce982fa44266a4be925edf0dea2d1bae6
sasasanono <ukakau@gmail.com>
2026-01-03 16:00:00 +0900

    merge

--- scripts/runHorizonAtlasBuild.js
+++ scripts/runHorizonAtlasBuild.js
@@ -17,28 +17,29 @@ async function notifyDiscord(message) {
 }
 
 const token = process.env.HORIZON_ATLAS_APP_GH_PAT;
+(async()=>{
+    const res = await fetch(
+    "https://api.github.com/repos/Ryukoku-Horizon/horizon-atlas/actions/workflows/build_Deploy.yml/dispatches",
+    {
+        method: "POST",
+        headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ref: "main" }),
+    }
+    );
 
-const res = await fetch(
-  "https://api.github.com/repos/Ryukoku-Horizon/horizon-atlas/actions/workflows/build_Deploy.yml/dispatches",
-  {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: "application/vnd.github+json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ref: "main" }),
-  }
-);
+    if (!res.ok) {
+    const text = await res.text();
+    await notifyDiscord(`
+        notion2atlasからhorizon-atlasのworkflow実行に失敗しました
+        期限切れの可能性があるので、確認しましょう
+        error: ${text}
+        `)
+    console.error(text)
+    }
 
-if (!res.ok) {
-  const text = await res.text();
-  await notifyDiscord(`
-      notion2atlasからhorizon-atlasのworkflow実行に失敗しました
-      期限切れの可能性があるので、確認しましょう
-      error: ${text}
-    `)
-  console.error(text)
-}
-
-console.log("workflow dispatched");
+    console.log("workflow dispatched");
+})()
